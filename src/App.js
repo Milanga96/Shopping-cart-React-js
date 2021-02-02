d0fd269f48ff5ba01fb23faca8402dc0d5bbb9e7
Milanga96 <milangamanukalpana1996@gmail.com>
2021-02-02 19:20:49 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+const App = () =>{
+  return(
+    <div>
+        <nav className="M2">
+          <a href="#">LOgin</a>
+          <a href="#">Search bar</a>
+          </nav>
+      
+    </div>
+  )
+};
+
+export default App;
